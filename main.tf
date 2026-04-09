73f6c467891bc33cf65628ce45867454daf9819d
ouiam ait taleb <aittalebouiam@gmail.com>
2026-04-09 20:08:35 +0200

    fix: rename IAM role to avoid EntityAlreadyExists conflict

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "aws_iam_policy_document" "lambda_assume_role" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name               = "${var.function_name}-exec-role"
+  name               = "${var.function_name}-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
